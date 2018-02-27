ee09269b2093c12eee1e3d79273bc2d0bfd39600
Olavi Haapala <olavi.haapala@futurice.com>
2018-02-27 13:26:59 +0200

    Add BrowserStack for available slots and respect that

--- scripts/test.js
+++ scripts/test.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const webdriver = require('selenium-webdriver');
+const BrowserStack = require('browserstack-client');
 const { map } = require('lodash');
 
 const createInfluxDbClient = require('../utils/influxDbClient');
@@ -9,7 +10,7 @@ const {
   INLUX_DB_NAME,
   INFLUX_USER,
   INFLUX_PASSWORD,
-  BROWSERSTACK_USER,
+  BROWSERSTACK_USERNAME,
   BROWSERSTACK_KEY,
   BROWSERSTACK_PROJECT,
   BROWSERSTACK_BUILD,
@@ -26,20 +27,20 @@ const client = createInfluxDbClient(
   INFLUX_DB_CREDENTIALS
 );
 
+// BrowserStack Automate API
+const automateClient = new BrowserStack.AutomateClient({
+  username: BROWSERSTACK_USERNAME,
+  key: BROWSERSTACK_KEY
+});
+
 const rounds = process.argv[2] ? process.argv[2] : 3,
   repeatViewCount = process.argv[3] ? process.argv[3] : 6,
   endPoints = TEST_ENDPOINTS.split(' '),
-  testEnvs = TEST_ENVS.split(' '),
-  maxParallel = 5,
-  maxQueued = 5;
-
-let queuedCount = 0;
-
-console.log(PRODUCT, endPoints, testEnvs, rounds, repeatViewCount);
+  testEnvs = TEST_ENVS.split(' ');
 
 // Input capabilities
 const capabilities = {
-  'browserstack.user': BROWSERSTACK_USER,
+  'browserstack.user': BROWSERSTACK_USERNAME,
   'browserstack.key': BROWSERSTACK_KEY,
   'browserstack.debug': 'true',
   project: BROWSERSTACK_PROJECT,
@@ -182,12 +183,12 @@ async function doRun({ testEnv, url, capabilities, path, queued }) {
 const generateTests = () => {
   let pending = [];
   for (let i = 0; i < rounds; i++) {
-    console.log(`Starting round ${i}`);
+    console.log(`Round ${i}`);
     for (const capabilities of capabilitiesUsed) {
       console.log(capabilities);
       for (const testEnv of testEnvs) {
         console.log(
-          `Running test for ${testEnv} on ${capabilities.device} ${
+          `Test for ${testEnv} on ${capabilities.device} ${
             capabilities.platform
           } ${capabilities.os_version} browser ${capabilities.browserName}`
         );
@@ -208,28 +209,52 @@ const generateTests = () => {
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-async function processTests(tests) {
+async function getStatus() {
+  return automateClient
+    .getStatus()
+    .then(status => {
+      console.log('Status', status);
+      /* {
+        automate_plan: 'Automate Mobile',
+        parallel_sessions_running: 0,
+        team_parallel_sessions_max_allowed: 5,
+        parallel_sessions_max_allowed: 5,
+        queued_sessions: 0,
+        queued_sessions_max_allowed: 5
+      } */
+      return status;
+    })
+    .catch(e => {
+      console.error(e);
+      return null;
+    });
+}
+
+async function runAsManyAsPossible(tests) {
   let pending = JSON.parse(JSON.stringify(tests));
 
-  // First run as many parallel tests as possible
-  for (let i = 0; i < maxParallel; i++) {
+  const status = await getStatus();
+  const availableParallel =
+      status.team_parallel_sessions_max_allowed -
+      status.parallel_sessions_running,
+    availableQueue =
+      status.queued_sessions_max_allowed - status.queued_sessions;
+
+  // Run and queue as many parallel tests as possible
+  for (let i = 0; i < availableParallel + availableQueue; i++) {
     const testToRun = pending.pop();
     doRun({ ...testToRun, queued: false });
   }
+  return pending;
+}
 
-  // Then queue the rest
-  while (pending.length > 0) {
-    if (queuedCount < maxQueued) {
-      queuedCount++;
-      const testToRun = pending.pop();
-      doRun({ ...testToRun, queued: true });
-    } else {
-      console.log(
-        `Going to sleep for 15s since there are ${queuedCount} tests already in the queue`
-      );
-      await sleep(15000);
-    }
-  }
+async function processTests(tests) {
+  let pending = JSON.parse(JSON.stringify(tests));
+  do {
+    pending = await runAsManyAsPossible(pending);
+    console.log(`Going to sleep for 15s and waiting for queue to get emptier`);
+    await sleep(15000);
+  } while (pending.length > 0);
   console.log('All tests are now running, pending or done.');
 }
 
